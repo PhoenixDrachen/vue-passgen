bbec7e8440deea6a10e4e97aa0a66e300d29114a
Tyler 'PhoenixDrachen' Behunin <phoenixdrachen22@gmail.com>
2020-09-09 01:06:01 -0600

    Created Files Needed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Generator />
+    <PastPasswords/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Generator from './components/Generator.vue';
+import PastPasswords from './components/PastPasswords.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Generator,
+    PastPasswords,
   },
 };
 </script>
---
+++ src/components/PastPasswords.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="pastPasswords">
+<p>This is where the previous passwords will be.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
