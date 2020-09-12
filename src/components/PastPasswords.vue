c8591167705a1b809671da12b263b829b0f7e335
Tyler 'PhoenixDrachen' Behunin <phoenixdrachen22@gmail.com>
2020-09-12 01:23:45 -0600

    minor changes

--- src/components/PastPasswords.vue
+++ src/components/PastPasswords.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="pastPasswords">
-<p>This is where the previous passwords will be.</p>
+    <span v-for="pass in pastPasswords" :key="pass">{{pass}}</span>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
   setup() {
-    return {};
+    const pastPasswords = ref(['lfdafjlks', 'klfjdasljfdsjlka', 'fldasjlfksa']);
+    return {
+      pastPasswords,
+    };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
+  #pastPasswords{
+    // display:grid;
+    width:90%;
+    display:flex;
+    justify-content: space-between;
+    margin:0 auto;
+  }
 </style>
